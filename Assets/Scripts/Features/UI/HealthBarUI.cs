361cf12cc54089b11f9af69cca428c813ddb5d0c
jwoo84 <jwoo84@gatech.edu>
2026-03-03 11:23:47 -0500

    Adding [ HealthBarUI ] reactivity after damage

--- Assets/Scripts/Features/UI/HealthBarUI.cs
+++ Assets/Scripts/Features/UI/HealthBarUI.cs
@@ -3,11 +3,17 @@ namespace Features.UI
     using UnityEngine;
     using UnityEngine.UI;
     using Features.Gameplay.Entities.Player;
+    using System.Collections;
+    using UnityEngine.Rendering;
 
     public class HealthBarUI : MonoBehaviour
     {
         [SerializeField] private PlayerHealth health;
         [SerializeField] private Image fill; // Image.type = Filled (Horizontal)
+        [SerializeField] private Color32 origFillColor = new Color32(29, 140, 1, 255);
+        [SerializeField] private float hpDrainSpeed = 3.0f;
+
+        private Coroutine hpDrainCoroutine;
 
         void Reset()
         {
@@ -31,7 +37,22 @@ namespace Features.UI
         void HandleChanged(int current, int max)
         {
             if (!fill) return;
-            fill.fillAmount = max > 0 ? Mathf.Clamp01((float)current / max) : 0f;
+            //fill.fillAmount = max > 0 ? Mathf.Clamp01((float)current / max) : 0f;
+            float end = max > 0 ? Mathf.Clamp01((float)current / max) : 0f;
+            if (hpDrainCoroutine != null) StopCoroutine(hpDrainCoroutine);
+            hpDrainCoroutine = StartCoroutine(HealthDrainAnimation(end));
+        }
+
+        IEnumerator HealthDrainAnimation(float end)
+        {
+            fill.color = Color.red;
+            while (fill.fillAmount - end > 0.001f)
+            {
+                fill.fillAmount = Mathf.Lerp(fill.fillAmount, end, Time.deltaTime * hpDrainSpeed);
+                yield return null;
+            }
+            fill.color = origFillColor;
+            fill.fillAmount = end;
         }
     }
 
