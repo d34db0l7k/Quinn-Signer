f79d3b71d605b0eb26644938446db2b6579d2a3b
Bridgette-Castronovo <145254750+Bridgette-Castronovo@users.noreply.github.com>
2026-03-16 22:51:19 -0400

    made game slow to 20% when question mark is pressed.

---
+++ Assets/Scripts/SlowMotionHelper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SlowMotionHelper : MonoBehaviour
+{
+    [SerializeField] private float slowTimeScale = 0.2f;
+    private bool isSlowed = false;
+
+    public void ToggleSlowMotion()
+    {
+        if (isSlowed)
+        {
+            Time.timeScale = 1f;
+            isSlowed = false;
+        }
+        else
+        {
+            Time.timeScale = slowTimeScale;
+            isSlowed = true;
+        }
+    }
+}
